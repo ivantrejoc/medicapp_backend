a6eb8aea2db33640a8d1ad0ffd3f81e2402fea33
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-08-16 08:51:35 -0500

    sequelize/postgres DB connection done

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import server from "./src/app.js";
+import sequelize from "./src/dbConnection.js";
 
 const PORT = 3001;
 
-server.listen(PORT, () => {
-  console.log("Server raised at port: " + PORT);
-});
+const runServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("DB Connection has been established successfully.");
+    server.listen(PORT, () => {
+      console.log("Server raised at port: " + PORT);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+runServer();
