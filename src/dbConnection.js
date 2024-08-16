1987188f60404ca3147159a5ed90b0cb278f8f77
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-08-16 09:58:23 -0500

    patient model done

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import Patient from "./models/Patient.js";
 
 dotenv.config();
-
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(
+export const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 );
 
-export default sequelize;
+export const patientModel = Patient(sequelize);
+
+
---
+++ src/models/Patient.js
@@ -0,0 +1,45 @@
+import { DataTypes } from "sequelize";
+
+const Patient = (sequelize) => {
+  return sequelize.define(
+    "Patient",
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        default: true,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    { timestamps: true }
+  );
+};
+
+export default Patient;
